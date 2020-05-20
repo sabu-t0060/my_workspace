72bd4be15fb01db734c7362a3f3a6a6cd9a5fad9
tobit sabu <douglastob12@gmail.com>
2020-05-19 17:32:12 -0700

    new content has been added

--- src/myjavaproject/myAppTester.java
+++ src/myjavaproject/myAppTester.java
@@ -16,15 +16,19 @@ public class myAppTester {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		int num;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Hello welcome to software engineering");
+		System.out.println("Enter the number to find the sum");
 		num = input.nextInt();
+		System.out.println("Sum of digits = "+sumOfDigits(num));
+		
 		
 		
 
 	}
-    public int sumOfDigits(int n) {
+    public static int sumOfDigits(int n) {
     	int sum =0,d;
     	while(n > 0)
         {
