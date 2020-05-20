7e00e19c744fedc1cc2b6ba65cb3948ef1d9f9c1
tobit sabu <douglastob12@gmail.com>
2020-05-19 17:24:15 -0700

    new content has been added

--- src/myjavaproject/myAppTester.java
+++ src/myjavaproject/myAppTester.java
@@ -3,6 +3,8 @@
  */
 package myjavaproject;
 
+import java.util.Scanner;
+
 /**
  * @author Tobit Sabu
  *
@@ -14,8 +16,22 @@ public class myAppTester {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num;
+		Scanner input = new Scanner(System.in);
 		System.out.println("Hello welcome to software engineering");
+		//num = input.nextInt();
+		
+		
 
 	}
-
+    public int sumOfDigits(int n) {
+    	int sum =0,d;
+    	while(n > 0)
+        {
+            d = n % 10;
+            sum = sum + d;
+            n = n / 10;
+        }
+    	return sum;
+    }
 }
