c45d3d4923e01800ee4f32ae631a93e0c7e233b0
tobit sabu <douglastob12@gmail.com>
2020-05-19 17:16:56 -0700

    new content has been added

---
+++ src/myjavaproject/myAppTester.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package myjavaproject;
+
+/**
+ * @author Tobit Sabu
+ *
+ */
+public class myAppTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello welcome to software engineering");
+
+	}
+
+}
